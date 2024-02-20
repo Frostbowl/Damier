6bdea2bf6afee7343343fa7c2a33026938bcb0fb
Frostbowl <masclet.c@gmail.com>
2024-02-20 21:48:50 +0400

    continue Damier

--- assets/app.js
+++ assets/app.js
@@ -1,44 +1,49 @@
 let body = document.body;
 let table = document.createElement('table');
-let h1 = document.querySelector('h1');
+body.appendChild(table);
 
 let letter = ["A", "B", "C", "D", "E", "F", "G", "H"];
-let number = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let number = [1, 2, 3, 4, 5, 6, 7, 8];
 
-function damier() {
 
-for (let i = 0; i < 8; i++) {
-    let ligne = document.createElement('th');
+for (let i = 0; i < 9; i++) {
+    let ligne = document.createElement('tr');
     table.appendChild(ligne);
-    if (i % 2 === 0) {
-        ligne.style.backgroundColor = 'black';
-        ligne.style.border = "solid black 1px";
-        ligne.style.padding = "11px";
-    } else {
-        ligne.style.backgroundColor = 'white';
-        ligne.style.border = "solid black 1px";
-        ligne.style.padding = "11px";
-    }
+    ligne.style.display = 'flex';
+    ligne.style.flexDirection = 'row';
 
-}
-for (let y = 0; y < number.length; y++) {
-    let colonne = document.createElement('tr');
-    table.appendChild(colonne);
-    if (y % 2 ===  0) {
-        colonne.style.backgroundColor = 'white';
-        colonne.style.border = 'solid black 1px';
-        colonne.style.padding = "11px";
-    } else {
-        colonne.style.backgroundColor = 'black';
-        colonne.style.border = 'solid black 1px';
-        colonne.style.padding = "11px";
-    }
-    
-}};
-damier();
-body.appendChild(table);
-body.style.display = 'flex';
+    for (let y = 0; y < 9; y++){
+        let cellule = document.createElement('td');
+        ligne.appendChild(cellule);
+
+        if (y + i % 2 === 0 ) {
+            cellule.style.backgroundColor = 'black'
+        } else {
+            cellule.style.backgroundColor = 'white'
+        };
+
+    };
+
+};
+
+table.style.flexDirection = 'row';
+
+body.style.display =  'flex';
 body.style.flexDirection = 'column';
-body.style.alignItems = 'center';
+body.style.alignItems = 'center'
 
 
+table.style.border = 'solid 1px black';
+
+/*    if (i % 2 === 0) {
+        let cellA = document.createElement('td');
+        ligne.appendChild(cellA);
+        cellA.style.backgroundColor = 'black';
+        cellA.style.padding = "11px";
+    } else {
+        let cellB = document.createElement('td');
+        ligne.appendChild(cellB);
+        cellB.style.backgroundColor = 'white';
+        cellB.style.padding = "11px";
+    }
+*/
